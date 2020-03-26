0a7c5e865ba42f5c8b04c8533fb97c4d4bbb3c25
zamoroz <moras8465132@gmail.com>
2020-03-26 19:41:19 +0700

    Связываем поля в макете с соответствующими переменными в коде приложения (MainActivity)

--- app/src/main/java/com/example/lr2/MainActivity.java
+++ app/src/main/java/com/example/lr2/MainActivity.java
@@ -21,5 +21,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_amount = findViewById(R.id.et_amount);
+        sb_percent = findViewById(R.id.sb_percent);
+        tv_percent = findViewById(R.id.tv_percent);
+        tv_tip = findViewById(R.id.tv_tip);
+        tv_total = findViewById(R.id.tv_total);
+
+        tv_tip.setText("0.0");
+        tv_total.setText("0.0");
+
     }
 }
