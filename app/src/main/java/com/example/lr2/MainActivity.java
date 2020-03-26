3d41bcf854c0e3de06dbc680ae01f58fda5f674e
zamoroz <moras8465132@gmail.com>
2020-03-26 19:32:54 +0700

    Описываем переменные в MainActivity

--- app/src/main/java/com/example/lr2/MainActivity.java
+++ app/src/main/java/com/example/lr2/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.lr2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private double amount = 0.0; // Сумма счёта
+    private double percent = 0.15; // Процент чаевых по умолчанию.
+    private EditText et_amount; // Поле для ввода суммы счёта
+    private SeekBar sb_percent; // Ползунок для процентов
+    private TextView tv_percent; // Поле для значения процента
+    private TextView tv_tip; // Поле для суммы чаевых
+    private TextView tv_total; // Поле для итоговой суммы
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
