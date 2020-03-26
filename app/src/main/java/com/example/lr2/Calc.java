9e0ace982e4565ffa30dd15ddb24d0cdc557a870
zamoroz <moras8465132@gmail.com>
2020-03-26 19:39:38 +0700

    Методы класса

--- app/src/main/java/com/example/lr2/Calc.java
+++ app/src/main/java/com/example/lr2/Calc.java
@@ -7,4 +7,38 @@ public class Calc {
     private double tip;
     private double total;
 
+    public double calculateTip(double billAmount, double percent) {
+        return billAmount*percent;
+    }
+    public double calculateTotal(double billAmount, double percent) {
+        return billAmount*(1+percent);
+    }
+
+    public double getBillAmount() {
+        return billAmount;
+    }
+    public void setBillAmount(double billAmount) {
+        this.billAmount = billAmount;
+    }
+
+    public double getPercent() {
+        return percent;
+    }
+    public void setPercent(double percent) {
+        this.percent = percent;
+    }
+
+    public double getTip() {
+        return tip;
+    }
+    public void setTip(double tip) {
+        this.tip = tip;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+    public void setTotal(double total) {
+        this.total = total;
+    }
 }
